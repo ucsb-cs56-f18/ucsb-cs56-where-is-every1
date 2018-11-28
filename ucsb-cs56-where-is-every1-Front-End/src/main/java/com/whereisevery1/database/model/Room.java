efa2d1a572c54aadd39f7c47245b86210232cb6e
jdavalos1 <43660774+jdavalos1@users.noreply.github.com>
2018-11-28 12:58:22 -0800

    JD: Commiting scraping along with front end
    Requires changes to regex in the Room class so that it fixes the time as military time (add 12 to the time if pm). Create the JSON writing method reading method. Needs a Time/Date class to hold the times by days and then times; this will need the Room class to be changed to allow for this change.

---
+++ ucsb-cs56-where-is-every1-Front-End/src/main/java/com/whereisevery1/database/model/Room.java
@@ -0,0 +1,46 @@
+package com.whereisevery1.database.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+public class Room {
+
+    @Id
+    public long id;
+	
+    private HashMap<String,ArrayList<List<Integer>>> times_dates;
+    private int number;
+
+    public Room(int number) {
+    	this.times_dates = new HashMap<String,ArrayList<List<Integer>>>();
+        this.number = number;
+    }
+    
+    public int getNumber() {
+    	return number;
+    }
+    
+    public HashMap<String,ArrayList<List<Integer>>> getTimes(){
+    	return times_dates;
+    }
+    public void addTimesDates(String dates, String times) {
+    	String[] t = times.split(":|(?i)[a-z]{2}\\s*-*\\s*");
+    	String [] d = dates.split("\\s");
+    	
+    	for(String day : d) {
+    		if(this.times_dates.containsKey(day)){
+    			this.times_dates.get(day).add(Arrays.asList(Integer.parseInt(t[0]+t[1]), Integer.parseInt(t[2] + t[3])));
+    		}
+    		else {
+    			ArrayList<List<Integer>> temp = new ArrayList<List<Integer>>();
+    			temp.add(Arrays.asList(Integer.parseInt(t[0]+t[1]), Integer.parseInt(t[2] + t[3])));
+    			times_dates.put(day, temp);
+    		}
+    	}
+    }
+}
+
